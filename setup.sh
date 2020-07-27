695f412ee8c5a6e22941c8941bc929d1c19daa1a
Antony Vennard <antony@vennard.ch>
2020-07-27 16:34:41 +0200

    Setup builds AFL++ into aflplusplus

--- setup.sh
+++ setup.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+# Display all commands on the command line so users can see 
+# what we're doing and why
+set -ex
+
+# variables (how to find things) go here:
+AFLPP=$(pwd)/aflplusplus
+
+
+
+# build AFL++:
+echo "[*] Cloning AFL++ into $AFLPP"
+if !test -d $AFLPP; then
+    git clone https://github.com/AFLplusplus/AFLplusplus $AFLPP
+fi
+
+pushd $AFLPP
+git checkout "2.66c"
+
+echo "[*] Building AFL++"
+
+make || exit 1
+echo "[*] Building AFL++ qemu_mode support"
+pushd $AFLPP/qemu_mode
+./build_qemu_support.sh || exit 1
+popd && popd
+
+
